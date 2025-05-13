75ed7502312f8d7bba73337a61cc8fa6af2ec540
kulnel <nkulkar5@stevens.edu>
2025-05-12 20:22:38 -0400

    workout dashboard working

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import authRoutes from './auth_routes.js';
+import workouts from './workouts.js'
 
 const constructorMethod = (app) => {
   app.use('/', authRoutes);
+  app.use('/workouts', workouts);
 
   app.use((req, res) => {
     res.status(404).send('Not found');
--- routes/workouts.js
+++ routes/workouts.js
@@ -5,11 +5,21 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   const all = await getAllWorkouts();
-  res.render('workoutList', { workouts: all });
+  res.render('workoutDashboard', { workouts: all });
 });
 
 router.get('/form', async (req, res) => {
   res.render('workoutForm', {isEdit: false});
+});
+
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const workout = await getWorkoutById(req.params.id);
+    if (!workout) return res.status(404).send('Workout not found');
+    res.render('workoutForm', { workout, isEdit: true });
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 router.post('/add', async (req, res) => {
@@ -17,6 +27,26 @@ router.post('/add', async (req, res) => {
     const { username, group, time, exercises } = req.body;
     const parsedExercises = JSON.parse(exercises); // expects array of exercise objects
     await createWorkout(username, group, time, parsedExercises);
+    res.redirect('/workouts');
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post('/edit/:id', async (req, res) => {
+  try {
+    const { username, group, time, exercises } = req.body;
+    const parsedExercises = JSON.parse(exercises);
+    await updateWorkout(req.params.id, { username, group, time, exercises: parsedExercises });
+    res.redirect('/workouts');
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post('/delete/:id', async (req, res) => {
+  try {
+    await deleteWorkoutById(req.params.id);
     res.redirect('/workouts');
   } catch (e) {
     res.status(400).send(e);
