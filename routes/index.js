8fb1554d4e67be5a673f00e48ee904fd9183162c
arjund95 <arjundubewar1@gmail.com>
2025-05-13 09:27:06 +0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import authRoutes from './auth_routes.js';
 import workouts from './workouts.js'
+import calendarRoutes from './calendar.js';
 
 const constructorMethod = (app) => {
   app.use('/', authRoutes);
   app.use('/workouts', workouts);
+  app.use('/calendar', calendarRoutes);
 
   app.use((req, res) => {
     res.status(404).send('Not found');
