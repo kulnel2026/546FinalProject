389fa8a04f433714b2848c05add4b9643cd3c6a5
arjund95 <arjundubewar1@gmail.com>
2025-05-13 09:17:41 +0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import authRoutes from './auth_routes.js';
+import calendarRoutes from './calendar.js';
 
 const constructorMethod = (app) => {
   app.use('/', authRoutes);
+  app.use('/calendar', calendarRoutes);
 
   app.use((req, res) => {
     res.status(404).send('Not found');
