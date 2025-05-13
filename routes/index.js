0aae43af14f432ebae9ac306f5882ed73bad9acb
schunduru256 <146134857+schunduru256@users.noreply.github.com>
2025-05-13 17:49:07 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import authRoutes from './auth_routes.js';
 import workouts from './workouts.js'
 import calendarRoutes from './calendar.js';
+import mealTrackerRoutes from './mealTrackerRoutes.js'
 
 const constructorMethod = (app) => {
   app.use('/', authRoutes);
   app.use('/workouts', workouts);
   app.use('/calendar', calendarRoutes);
+  app.use('/meals', mealTrackerRoutes);
 
   app.use((req, res) => {
     res.status(404).send('Not found');
