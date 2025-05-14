10b5a87b181256cb44daa7f0e5ea95a02a0affd8
schunduru256 <146134857+schunduru256@users.noreply.github.com>
2025-05-13 20:59:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import Handlebars from 'handlebars';
 import workoutRoutes from './routes/workouts.js'
-import calendarRoutes from './routes/calendar.js'
-import statsRoutes from './routes/stats.js'
+import mealsRouter from './routes/mealTrackerRoutes.js';
+import goalTrackerRoute from './routes/goalTracker.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -44,7 +44,7 @@ Handlebars.registerHelper('json', function (context) {
    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', handlebarsInstance.engine);
 app.set('view engine', 'handlebars');
 
@@ -80,12 +80,11 @@ function requireAuth(req, res, next) {
      next();
 }
 app.use('/user', requireAuth);
+app.use('/meals', mealsRouter);
+
 
 app.use('/workouts', requireAuth, workoutRoutes);
-app.use('/calendar', requireAuth, calendarRoutes);
-app.use('/stats', requireAuth, statsRoutes);
-
-
+app.use('/goals', goalTrackerRoute);
 
 function requireSignout(req, res, next) {
      if (!req.session.user) return res.redirect('/login');
